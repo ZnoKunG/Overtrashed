e1914398a0955328b6211bd089b8a56a6e425717
ZnoKunG <zno.ksy@gmail.com>
2022-06-04 16:28:49 +0700

    Add main mechanics of the game
    Interact and pick up and drop item but no pickable item is created.

---
+++ Assets/InteractableController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Overtrashed.Model;
+using UnityEngine;
+
+namespace Overtrashed.Player
+{
+    public class InteractableController : MonoBehaviour
+    {
+        [SerializeField] private Transform playerPivot;
+        private readonly HashSet<Interactable> _interactables = new HashSet<Interactable>();
+
+        private void Awake()
+        {
+            if (playerPivot == null)
+            {
+                playerPivot = transform;
+            }
+        }
+
+        /// <summary>
+        /// Get the current highlighted interactable (in range). Null if there is none in range.
+        /// </summary>
+        
+        [CanBeNull]
+        public Interactable currentInteractable { get; private set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Interactable interactable = other.gameObject.GetComponent<Interactable>();
+            if (interactable == null) return;
+
+            if (_interactables.Contains(interactable))
+            {
+                Debug.LogWarning($"[InteractableController] TriggerEnter on a preexisting collider {other.gameObject.name}");
+                return;
+            }
+            _interactables.Add(interactable);
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            Interactable interactable = other.gameObject.GetComponent<Interactable>();
+           if (interactable != null)
+            {
+                _interactables.Remove(interactable);
+            }
+        }
+
+        public void Remove(Interactable interactable)
+        {
+            _interactables.Remove(interactable);
+        }
+
+        private void FixedUpdate()
+        {
+            Interactable closest = TryGetClosestInteractable();
+
+            if (closest == currentInteractable) return;
+
+            // Something has changed
+            currentInteractable?.ToggleHighlightOff(); // check if hand is holding any interactable?
+            currentInteractable = closest;
+
+            closest?.ToggleHighlightOn(); // Also check if there is nearby interactable object
+        }
+
+        private Interactable TryGetClosestInteractable()
+        {
+            var minDistance = float.MaxValue;
+            Interactable closest = null; // in case there is no interactable nearby
+            foreach (var interactable in _interactables)
+            {
+                var distance = Vector3.Distance(playerPivot.position, interactable.gameObject.transform.position);
+                if (distance > minDistance) continue;
+                minDistance = distance;
+                closest = interactable;
+            }
+
+            return closest;
+        }
+
+    }
+}
+
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,147 +1,207 @@
 using System;
 using System.Collections;
+using Overtrashed.Model;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Overtrashed.Player
 {
-    private Rigidbody rb;
-
-    private InputAction _moveAction;
-    private InputAction _interactAction;
-
-    private Vector3 _inputDirection;
-    private Vector2 inputMovement;
-    [SerializeField] float movementSpeed;
-    [SerializeField] float inputDelay;
-
-    private bool _hasSubscribedControllerEvents;
-    private bool _isActive;
-
-    private InputController inputController;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
+        private Rigidbody rb;
 
-        inputController = new InputController();
-        _interactAction = inputController.Player1.Interact;
-        _moveAction = inputController.Player1.Move;
-    }
+        private InputAction _moveAction;
+        private InputAction _interactAction;
+        private InputAction _pickupAction;
 
-    private void OnEnable()
-    {
-        inputController.Player1.Enable();
-        SubscribeControllerEvents();
-        SubscribeInteractableEvents();
-    }
+        private Vector3 _inputDirection;
+        private Vector2 inputMovement;
+        [SerializeField] float movementSpeed;
+        [SerializeField] float inputDelay;
 
-    private void OnDisable()
-    {
-        UnSubscribeControllerEvents();
-        UnSubscribeInteractableEvents();   
-    }
-    private void SubscribeControllerEvents()
-    {
-        if (_hasSubscribedControllerEvents) return;
+        private Transform slot;
 
-        _hasSubscribedControllerEvents = true;
-        //_moveAction.performed += HandleMove;
-        _interactAction.performed += HandleInteract;
-    }
+        private bool _hasSubscribedControllerEvents;
+        private bool _isActive;
+        private IPickable currentPickable;
 
-    private void UnSubscribeControllerEvents()
-    {
+        private InputController inputController;
 
-    }
+        private InteractableController interactableController;
 
-    private void SubscribeInteractableEvents()
-    {
-
-    }
-
-    private void UnSubscribeInteractableEvents()
-    {
-
-    }
-
-    private void Update()
-    {
-        CalculateInputDirection();
-    }
-
-    private void FixedUpdate()
-    {
-        MovePlayer();
-        TurnPlayer();
-        AnimatePlayerMovement();
-    }
-
-    private void MovePlayer()
-    {
-        rb.AddForce(_inputDirection * movementSpeed, ForceMode.Force);
-    }
-    private void HandleInteract(InputAction.CallbackContext context)
-    {
-        Debug.Log("Interact");
-    }
-
-    /*private void HandleMove(InputAction.CallbackContext context)
-    {
-        inputMovement = context.ReadValue<Vector2>();
-        OptimizeMoveInput();
-        _inputDirection = new Vector3(inputMovement.x, 0f, inputMovement.y);
-        Debug.Log("Handle Move : " + _inputDirection);
-    }*/
-
-    private void CalculateInputDirection()
-    {
-        inputMovement = _moveAction.ReadValue<Vector2>();
-        OptimizeMoveInput();
-        Debug.Log("CalculateInputDirection : " + _inputDirection);
-    }
-
-    private void OptimizeMoveInput()
-    {
-
-        if (inputMovement.x > inputDelay)
+        private void Awake()
         {
-            inputMovement.x = 1.0f;
-        }
-        else if (inputMovement.x < -inputDelay)
-        {
-            inputMovement.x = -1.0f;
-        }
-        else
-        {
-            inputMovement.x = 0f;
+            rb = GetComponent<Rigidbody>();
+
+            inputController = new InputController();
+            _interactAction = inputController.Player1.Interact;
+            _moveAction = inputController.Player1.Move;
+            _pickupAction = inputController.Player1.PickUp;
+
+            interactableController = GetComponentInChildren<InteractableController>();
+            slot = transform.Find("Slot");
+            
         }
 
-        if (inputMovement.y > inputDelay)
+        private void OnEnable()
         {
-            inputMovement.y = 1.0f;
+            inputController.Player1.Enable();
+            SubscribeControllerEvents();
+            SubscribeInteractableEvents();
         }
-        else if (inputMovement.y < -inputDelay)
+
+        private void OnDisable()
         {
-            inputMovement.y = -1.0f;
+            UnSubscribeControllerEvents();
+            UnSubscribeInteractableEvents();
         }
-        else
+        private void SubscribeControllerEvents()
         {
-            inputMovement.y = 0f;
+            if (_hasSubscribedControllerEvents) return;
+
+            _hasSubscribedControllerEvents = true;
+            //_moveAction.performed += HandleMove;
+            _interactAction.performed += HandleInteract;
+            _pickupAction.performed += HandlePickUp;
         }
-        _inputDirection = new Vector3(inputMovement.x, 0f, inputMovement.y);
-    }
-    private void TurnPlayer()
-    {
-        if (!(rb.velocity.magnitude > 0.1f) || _inputDirection == Vector3.zero) return;
 
-        Quaternion newRotation = Quaternion.LookRotation(_inputDirection);
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 15f);
+        private void UnSubscribeControllerEvents()
+        {
+            if (_hasSubscribedControllerEvents == false) return;
 
+            _hasSubscribedControllerEvents = false;
+            _interactAction.performed -= HandleInteract;
+            _pickupAction.performed -= HandlePickUp;
+        }
+
+        private void SubscribeInteractableEvents()
+        {
+
+        }
+
+        private void UnSubscribeInteractableEvents()
+        {
+
+        }
+
+        private void Update()
+        {
+            CalculateInputDirection();
+        }
+
+        private void FixedUpdate()
+        {
+            MovePlayer();
+            TurnPlayer();
+            AnimatePlayerMovement();
+        }
+
+        private void MovePlayer()
+        {
+            rb.AddForce(_inputDirection * movementSpeed, ForceMode.Force);
+        }
+        private void HandleInteract(InputAction.CallbackContext context)
+        {
+            Debug.Log("Interact");
+            interactableController.currentInteractable?.Interact(this);
+        }
+
+        private void HandlePickUp(InputAction.CallbackContext context)
+        {
+            var interactable = interactableController.currentInteractable;
+
+            // Empty hand
+            if (currentPickable == null)
+            {
+                currentPickable = interactable as IPickable;
+
+                // Pickable item
+                if (currentPickable != null)
+                {
+                    // Set PickUp Animation
+                    // Play PickUp Sound
+                    currentPickable.Pick();
+                    interactableController.Remove(currentPickable as Interactable);
+                    currentPickable.gameObject.transform.SetPositionAndRotation(slot.position, Quaternion.identity);
+                    currentPickable.gameObject.transform.SetParent(slot);
+                    return;
+                }
+
+                // Interactable ONLY ( not pickable )
+                currentPickable = interactable?.TryToPickUpFromSlot(currentPickable);
+                if (currentPickable != null)
+                {
+                    // Set PickUp Animation
+                    // Play PickUp Sound
+                    currentPickable.gameObject.transform.SetPositionAndRotation(slot.position, Quaternion.identity);
+                    currentPickable.gameObject.transform.SetParent(slot);
+                    return;
+                }
+
+            }
+
+            // Carrying a Pickable, try to drop
+            // No interactable in range or holding interactable
+            if (interactable == null || interactable is IPickable)
+            {
+                // SetUp Drop (Back to idle) Animation
+                // Play Drop Sound
+                currentPickable.Drop();
+                currentPickable = null;
+                return;
+            }
+        }
+
+        private void CalculateInputDirection()
+        {
+            inputMovement = _moveAction.ReadValue<Vector2>();
+            OptimizeMoveInput();
+            Debug.Log("CalculateInputDirection : " + _inputDirection);
+        }
+
+        private void OptimizeMoveInput()
+        {
+
+            if (inputMovement.x > inputDelay)
+            {
+                inputMovement.x = 1.0f;
+            }
+            else if (inputMovement.x < -inputDelay)
+            {
+                inputMovement.x = -1.0f;
+            }
+            else
+            {
+                inputMovement.x = 0f;
+            }
+
+            if (inputMovement.y > inputDelay)
+            {
+                inputMovement.y = 1.0f;
+            }
+            else if (inputMovement.y < -inputDelay)
+            {
+                inputMovement.y = -1.0f;
+            }
+            else
+            {
+                inputMovement.y = 0f;
+            }
+            _inputDirection = new Vector3(inputMovement.x, 0f, inputMovement.y);
+        }
+        private void TurnPlayer()
+        {
+            if (!(rb.velocity.magnitude > 0.1f) || _inputDirection == Vector3.zero) return;
+
+            Quaternion newRotation = Quaternion.LookRotation(_inputDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 15f);
+
+        }
+
+        private void AnimatePlayerMovement()
+        {
+            // Animate PlayerMovement
+        }
     }
 
-    private void AnimatePlayerMovement()
-    {
-        // Animate PlayerMovement
-    }
 }
