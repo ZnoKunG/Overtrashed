f1a0b93aa01f219b6f98524c478ce93170b333de
ZnoKunG <zno.ksy@gmail.com>
2022-06-04 17:03:13 +0700

    Trash types Script Added
    Trash can be picked and dropped and have system to change the status of the trash through codes.

---
+++ Assets/TrashStatus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Overtrashed.Model
+{
+    public enum TrashStatus
+    {
+        Raw,
+        Wet,
+        Dirty,
+        NotSorted,
+    }
+}
